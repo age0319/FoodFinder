66c88c8b9673b72b254cec18a7237e50271f05b0
ha <ha@ex.com>
2020-06-15 07:46:27 +0900

    ストーリーボードにテキストフィールドとマップを作成。

--- MapFinder/ViewController.swift
+++ MapFinder/ViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputText: UITextField!
+    @IBOutlet weak var dispMap: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
