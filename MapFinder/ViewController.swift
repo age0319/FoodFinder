22e23899e95e03d4a8bd88416f203a9134dae2fe
ha <ha@ex.com>
2020-06-15 13:43:36 +0900

    FoursquareAPIClientを導入。

--- MapFinder/ViewController.swift
+++ MapFinder/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import FoursquareAPIClient
 
 class ViewController: UIViewController, UITextFieldDelegate {
 
@@ -18,6 +19,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         inputText.delegate = self
+        setup()
     }
 
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -51,6 +53,39 @@ class ViewController: UIViewController, UITextFieldDelegate {
         }
         
         return true
+    }
+    
+    func setup(){
+        let client = FoursquareAPIClient(clientId: "JN0UJMZ0JDVEPJ3B1N2BTHYDUBZ1LLOXC0ZVEEWZZL5UXLPD",
+        clientSecret: "5MXF4GNXS3QZTNW0YSLQRHG5LWY2RWKA1SKHAAJDBS2OSJ3F")
+        
+        let parameter: [String: String] = [
+            "ll": "35.702069,139.7753269", // 緯度経度
+            "limit": "1", // 一度に取得する件数
+        ];
+        
+        client.request(path: "venues/search", parameter: parameter) {
+            result in
+
+            switch result {
+
+            case .success(let data):
+                // 成功
+                print(NSString(data:data, encoding:String.Encoding.utf8.rawValue)!)
+
+            case .failure(.connectionError(let error)):
+                // 通信エラー
+                print(error)
+
+            case .failure(.apiError(let error)):
+                // 通信はできたけどAPIエラー
+                print(error.errorType)   // e.g. endpoint_error
+                print(error.errorDetail) // e.g. The requested path does not exist.
+                
+            case .failure(.responseParseError(_)): break
+    
+            }
+        }
     }
 }
 
