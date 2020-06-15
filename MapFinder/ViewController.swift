4ca4c00f851a0d15cce9f244ec8989c4232a1521
ha <ha@ex.com>
2020-06-15 09:45:48 +0900

    緯度と経度を検索してピンを立てる機能を実装。

--- MapFinder/ViewController.swift
+++ MapFinder/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var inputText: UITextField!
     @IBOutlet weak var dispMap: MKMapView!
@@ -17,8 +17,40 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        inputText.delegate = self
     }
 
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        if let searchKey = textField.text{
+            print(searchKey)
+        
+            let geocoder = CLGeocoder()
+        
+            geocoder.geocodeAddressString(searchKey, completionHandler: { (placemarks, error) in
+            
+                if let unwrapPlacemarks = placemarks{
+                    if let firstPlacemark = unwrapPlacemarks.first{
+                        if let location = firstPlacemark.location{
+                            let coordinate = location.coordinate
+                            print(coordinate)
+                            
+                            let pin = MKPointAnnotation()
+                            
+                            pin.coordinate = coordinate
+                            
+                            pin.title = searchKey
+                            
+                            self.dispMap.addAnnotation(pin)
+                            self.dispMap.region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 500.0, longitudinalMeters: 500.0)
+                        }
+                    }
+                }
+            })
+        }
+        
+        return true
+    }
 }
 
