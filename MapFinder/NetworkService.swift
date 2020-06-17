badcbd1b73965a6bce901535bd7962032688d9ba
ha <ha@ex.com>
2020-06-17 15:13:16 +0900

    設定画面を作った。

---
+++ MapFinder/NetworkService.swift
@@ -0,0 +1,74 @@
+//
+//  NetworkService.swift
+//  MapFinder
+//
+//  Created by haru on 2020/06/17.
+//  Copyright © 2020 Harunobu Agematsu. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class NetworkService {
+    
+    var venueList:[(name:String, lat:Double, lng:Double, category:String?)] = []
+
+    func searchAroundVenue(loc:CLLocationCoordinate2D,completion: @escaping ([(name:String, lat:Double, lng:Double, category:String?)]?) -> ()){
+        
+        let clientId = "JN0UJMZ0JDVEPJ3B1N2BTHYDUBZ1LLOXC0ZVEEWZZL5UXLPD"
+        let clientSecret = "5MXF4GNXS3QZTNW0YSLQRHG5LWY2RWKA1SKHAAJDBS2OSJ3F"
+        let loc = Utility().coordinateToString(loc: loc)
+        let radius = 500
+        let version = "20200601"
+        let query = ["sushi","noodle","soba","udon"]
+        
+        var url = "https://api.foursquare.com/v2/venues/explore?&client_id=" + clientId
+        url += "&client_secret=" + clientSecret
+        url += "&v=" + version
+        url += "&ll=" + loc
+        url += "&query=" + query[3]
+        url += "&radius=" + String(radius)
+        
+        let req_url = URL(string: url)
+        let req = URLRequest(url: req_url!)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        let dispatchGroup = DispatchGroup()
+
+        dispatchGroup.enter()
+        
+        let task = session.dataTask(with: req, completionHandler: {
+            (data, response, error) in
+            session.finishTasksAndInvalidate()
+            print(String(data:data!, encoding:String.Encoding(rawValue: String.Encoding.utf8.rawValue))!)
+            self.ParseJSON(data: data!)
+            dispatchGroup.leave()
+            })
+        
+        task.resume()
+
+        dispatchGroup.notify(queue: .main){
+            completion(self.venueList)
+        }
+    }
+
+    func ParseJSON(data:Data){
+        
+        do {
+            let decoder = JSONDecoder()
+            let json = try decoder.decode(ResultJson.self, from: data)
+            
+            let items = json.response.groups[0].items
+            
+            for item in items{
+                let venue = item.venue
+                let place = (venue.name,venue.location.lat,venue.location.lng,venue.categories[0].name)
+                venueList.append(place)
+            }
+            
+        } catch  {
+            print("エラー")
+        }
+            
+    }
+}
---
+++ MapFinder/SettingVC.swift
@@ -0,0 +1,45 @@
+//
+//  SettingVC.swift
+//  MapFinder
+//
+//  Created by haru on 2020/06/17.
+//  Copyright © 2020 Harunobu Agematsu. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+import MapKit
+
+class SettingVC: UIViewController , UIPickerViewDataSource, UIPickerViewDelegate{
+    
+    private let dataSource = ["寿司","蕎麦","うどん","ラーメン","カレー","焼肉"]
+    
+    var selected = ""
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        dataSource.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selected = dataSource[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return dataSource[row]
+    }
+    
+    override func viewDidLoad() {
+        pickerView.dataSource = self
+        pickerView.delegate = self
+    }
+    
+    
+    
+}
